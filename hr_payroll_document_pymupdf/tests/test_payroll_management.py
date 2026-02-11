cf208d7b2ccf498cebc5e36052a5e29534cac468
Simone Rubino <simone.rubino@pytech.it>
2026-02-11 12:22:53 +0100

    [MIG] hr_payroll_document_pymupdf: Migration to 17.0

--- hr_payroll_document_pymupdf/tests/test_payroll_management.py
+++ hr_payroll_document_pymupdf/tests/test_payroll_management.py
@@ -11,7 +11,7 @@ class TestPayrollManagement(TestHrPayrollDocument):
         with self._mock_valid_identification(self.employee_emp, identification_code):
             self.employee_emp.identification_id = identification_code
         self.wizard = self._create_wizard(
-            "Subject", ["hr_payroll_document_pymupdf", "tests", "test_broken_image.pdf"]
+            "Subject", "hr_payroll_document_pymupdf/tests/test_broken_image.pdf"
         )
         with self._mock_valid_identification(self.employee_emp, identification_code):
             result_action = self.wizard.send_payrolls()
